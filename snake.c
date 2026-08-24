c62501b082aecd961279192eac09b4e8d204746b
Eduardo Moreira <eduardomoreira222003@gmail.com>
2026-08-24 12:40:38 +0100

    [Sessão 3] Adiciona recursão à colisão da cobra

--- snake.c
+++ snake.c
@@ -1,95 +1,96 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include "game.h"
 #include "snake.h"
 
 void moverCobra(char direcao)
 {
-    int i;
+int i;
 
-    for (i = tamanhoCobra - 1; i > 0; i--)
-    {
-        cobra[i] = cobra[i - 1];
-    }
+for (i = tamanhoCobra - 1; i > 0; i--)
+    cobra[i] = cobra[i - 1];
 
-    switch (direcao)
-    {
-        case 'w':
-            cobra[0].y--;
-            break;
+switch (direcao)
+{
+    case 'w':
+        cobra[0].y--;
+        break;
 
-        case 's':
-            cobra[0].y++;
-            break;
+    case 's':
+        cobra[0].y++;
+        break;
 
-        case 'a':
-            cobra[0].x--;
-            break;
+    case 'a':
+        cobra[0].x--;
+        break;
 
-        case 'd':
-            cobra[0].x++;
-            break;
-    }
+    case 'd':
+        cobra[0].x++;
+        break;
+}
+
 }
 
 int verificarColisao()
 {
-    if (cobra[0].x <= 0 ||
-        cobra[0].x >= LARGURA - 1 ||
-        cobra[0].y <= 0 ||
-        cobra[0].y >= ALTURA - 1)
-    {
-        return 1;
-    }
+if (cobra[0].x <= 0 ||
+cobra[0].x >= LARGURA - 1 ||
+cobra[0].y <= 0 ||
+cobra[0].y >= ALTURA - 1)
+{
+return 1;
+}
 
-    return 0;
+return 0;
+
+}
+
+int verificarColisaoCorpoRecursiva(int indice)
+{
+if (indice >= tamanhoCobra)
+return 0;
+
+if (cobra[0].x == cobra[indice].x &&
+    cobra[0].y == cobra[indice].y)
+    return 1;
+
+return verificarColisaoCorpoRecursiva(indice + 1);
+
 }
 
 int verificarColisaoCorpo()
 {
-    int i;
-
-    for (i = 1; i < tamanhoCobra; i++)
-    {
-        if (cobra[0].x == cobra[i].x &&
-            cobra[0].y == cobra[i].y)
-        {
-            return 1;
-        }
-    }
-
-    return 0;
+return verificarColisaoCorpoRecursiva(1);
 }
 
 int verificarComida()
 {
-    if (cobra[0].x == comida.x &&
-        cobra[0].y == comida.y)
-    {
-        if (tamanhoCobra < TAM_MAX)
-        {
-            tamanhoCobra++;
-        }
+if (cobra[0].x == comida.x &&
+cobra[0].y == comida.y)
+{
+if (tamanhoCobra < TAM_MAX)
+tamanhoCobra++;
 
-        pontuacao += 10;
+    pontuacao += 10;
+    reposicionarComida();
 
-        reposicionarComida();
+    return 1;
+}
 
-        return 1;
-    }
+return 0;
 
-    return 0;
 }
 
 void reiniciarCobra()
 {
-    cobra[0].x = 20;
-    cobra[0].y = 10;
+cobra[0].x = 20;
+cobra[0].y = 10;
 
-    cobra[1].x = 19;
-    cobra[1].y = 10;
+cobra[1].x = 19;
+cobra[1].y = 10;
 
-    cobra[2].x = 18;
-    cobra[2].y = 10;
+cobra[2].x = 18;
+cobra[2].y = 10;
 
-    tamanhoCobra = 3;
+tamanhoCobra = 3;
+
 }
